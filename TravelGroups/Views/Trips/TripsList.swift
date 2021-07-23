68ee81d7e9994c4e331347e6365ad8b0dc0e4adb
Matthew Meyerink <meyerink@umich.edu>
2021-07-22 20:24:14 -0700

    Restructure The views folder

---
+++ TravelGroups/Views/Trips/TripsList.swift
@@ -0,0 +1,20 @@
+//
+//  TripsList.swift
+//  TravelGroups
+//
+//  Created by Matthew Meyerink on 7/22/21.
+//
+
+import SwiftUI
+
+struct TripsList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TripsList_Previews: PreviewProvider {
+    static var previews: some View {
+        TripsList()
+    }
+}
