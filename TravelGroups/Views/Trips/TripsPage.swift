3f7e18a13fd4d84859d44613c72d38dbe2f57863
Matthew Meyerink <meyerink@umich.edu>
2021-07-31 18:16:32 -0700

    Adding new trip icon to trips list page

--- TravelGroups/Views/Trips/TripsPage.swift
+++ TravelGroups/Views/Trips/TripsPage.swift
@@ -16,11 +16,23 @@ struct TripsPage: View {
         VStack {
             Text("Trips")
                 .font(.largeTitle)
-            Toggle(isOn: $showPastTrips) {
-                Text("Show past trips...")
-            }
+            
             Text("The Current Trip will go HERE!")
                 .font(.largeTitle)
+            
+            HStack(spacing: 50) {
+                Text("Show Past Trips")
+                Toggle(isOn: $showPastTrips){}
+                
+                Text("Add New Trip")
+                Button(action: {
+                    print("Create new Trip")
+                }) {
+                    Image(systemName: "plus")
+                }
+            }
+                .padding(.horizontal, 50)
+            
             if showPastTrips {
                 TripsList(trips: pastTrips, listTitle: "Past Trips")
             } else {
