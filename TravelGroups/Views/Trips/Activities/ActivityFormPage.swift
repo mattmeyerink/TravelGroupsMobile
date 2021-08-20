0c8b10fdd635f574281a5f8288a161c4062fea23
Matthew Meyerink <meyerink@umich.edu>
2021-08-20 00:33:27 -0400

    Add form data to activity form
    Populates the text form inputs on the activity form page

--- TravelGroups/Views/Trips/Activities/ActivityFormPage.swift
+++ TravelGroups/Views/Trips/Activities/ActivityFormPage.swift
@@ -10,8 +10,49 @@
 import SwiftUI
 
 struct ActivityFormPage: View {
+    @State var formActivityName: String = ""
+    @State var formActivityDescription: String = ""
+    
+    @State var formActivityStreet: String = ""
+    @State var formActivityCity: String = ""
+    @State var formActivityState: String = ""
+    @State var formActivityCountry: String = ""
+    @State var formActivityPostalCode: String = ""
+    
+    @State var price: Double = 0.00
+    
+    @State var formActivityUrl: String = ""
+    
+    @State var activitySubmitted: Bool = false
+    
     var body: some View {
-        Text("This is the activities Form Page")
+        Form {
+            Section(header: Text("NAME")) {
+                TextField("Name", text: $formActivityName)
+                TextField("Description", text: $formActivityDescription)
+            }
+            
+            Section(header: Text("ADDRESS")) {
+                TextField("Street", text: $formActivityStreet)
+                TextField("City", text: $formActivityStreet)
+                TextField("State", text: $formActivityState)
+                TextField("Country", text: $formActivityCountry)
+                TextField("Postal Code", text: $formActivityPostalCode)
+            }
+            
+            Section(header: Text("DETAILS")) {
+                TextField("URL", text: $formActivityUrl)
+            }
+            
+            Button(action: {activitySubmitted=true}, label: {
+                HStack {
+                    Spacer()
+                    Text("Save Activity")
+                    Spacer()
+                }
+            })
+        }
+            .navigationBarTitle("Add Activity")
     }
 }
 
